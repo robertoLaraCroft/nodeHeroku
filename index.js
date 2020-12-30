93dc5f05e3c028626a94adafc0571d4c5147cf25
robertoLaraCroft <76408482+robertoLaraCroft@users.noreply.github.com>
2020-12-30 17:26:10 +0100

    por defecto de heroku
    por defecto de heroku

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-const express = require("express");
-const app = express();
-const path = require('path');
-
-const indexRoute = require('./routes/index')
-
-// setiings
-app.set('port', 3000);
-app.set('views', path.join(__dirname, 'views'));
-// app.set('controller', path.join(__dirname, 'controller'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs' );
-
-// middlewares
-
-// routes
-app.use(indexRoute);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// listening the server
-app.listen(app.get('port'), () =>{
-    console.log('Server on port', app.get('port'));
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
